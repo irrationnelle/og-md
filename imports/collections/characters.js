21b3098fe0dbb76e2ba0e6fc41f8b792a33edde5
irrationnelle <drakkarverenis@gmail.com>
2017-08-26 21:45:19 +0900

    IMPLEMENTING: Character schema

--- imports/collections/characters.js
+++ imports/collections/characters.js
@@ -1,5 +1,17 @@
 import { Mongo } from 'meteor/mongo';
 
+const Characters = new Mongo.Collection('chars');
+
+Characters.schema = new SimpleSchema({
+    name: { type: String },
+    ace: { type: String },
+    mind: { type: [String] },
+    twin: { type: String },
+    stage: { type: Number },
+    route: { type: String },
+    starred: { type: [String] },
+});
+
 Meteor.methods({
     'chars.insert': function() {
         return Characters.insert({
@@ -11,4 +23,4 @@ Meteor.methods({
     }
 });
 
-export const Characters = new Mongo.Collection('chars');
+export default Characters;
