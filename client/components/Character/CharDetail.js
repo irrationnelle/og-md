67ae4dd129712be1c82d21c32248aed26c4171f3
irrationnelle <drakkarverenis@gmail.com>
2017-09-13 00:16:44 +0900

    ADD: character collections JSON

--- client/components/Character/CharDetail.js
+++ client/components/Character/CharDetail.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { withStyles } from 'material-ui/styles';
-import Card, { CardHeader ,CardActions, CardContent } from 'material-ui/Card';
+import Card, { CardHeader, CardActions, CardContent } from 'material-ui/Card';
 import Collapse from 'material-ui/transitions/Collapse';
 import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton'
+import IconButton from 'material-ui/IconButton';
 import FavoriteBorderIcon from 'material-ui-icons/FavoriteBorder';
 import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 import Typography from 'material-ui/Typography';
@@ -18,111 +18,107 @@ import Characters from '../../../imports/collections/characters';
 const accent = red['A400']; // #FF1744
 
 const styles = theme => ({
-    card: {
-        marginLeft: 4,
-        marginTop: 7,
-        minWidth: 275,
-    },
-    title: {
-        marginLeft: 20,
-        marginTop: 14,
-        fontSize: 17,
-    },
-    infoname: {
-        fontSize: 15,
-        color: accent,
-        marginTop: 10,
-    },
-    info: {
-        fontSize: 15,
-        marginTop: 10,
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        transition: theme.transitions.create('transform', {
-          duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    flexGrow: {
-        flex: '1 1 auto',
-    },
+  card: {
+    marginLeft: 4,
+    marginTop: 7,
+    minWidth: 275,
+  },
+  title: {
+    marginLeft: 20,
+    marginTop: 14,
+    fontSize: 17,
+  },
+  infoname: {
+    fontSize: 15,
+    color: accent,
+    marginTop: 10,
+  },
+  info: {
+    fontSize: 15,
+    marginTop: 10,
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  flexGrow: {
+    flex: '1 1 auto',
+  },
 });
 
 const propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 const defaultProps = {
-    classes: undefined,
+  classes: undefined,
 };
 
 class CharDetail extends Component {
-    state = { expanded: false };
-    
-    handleExpandClick = () => {
-        this.setState({ expanded: !this.state.expanded });
-    };
+  state = { expanded: false };
 
-    render() {
-        const classes = this.props.classes;
-        const aceBonus = <span className={classes.infoname}>AB: </span>
-        const mindCommand = <span className={classes.infoname}>정신: </span>
-        const twinCommand = <span className={classes.infoname}>트윈: </span>
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
 
-        const char = this.props.data;
-        char.mind = char.mind.map((mind, i) => {
-            return (<span key={i}> {mind} </span>);
-        });
+  render() {
+    const classes = this.props.classes;
+    const aceBonus = <span className={classes.infoname}>AB: </span>;
+    const mindCommand = <span className={classes.infoname}>정신: </span>;
+    const twinCommand = <span className={classes.infoname}>트윈: </span>;
 
-        return (
-            // TODO: MAKE CHAR DETAIL COMPONENT AND TRANSFER KEY PROPS
-            <div>
-                <Card className={classes.card}>
-                    <CardActions>
-                        <Typography paragraph type="headline" className={classes.title}>
-                            {char.name}
-                        </Typography>
-                        <div className={classes.flexGrow} />
-                        <IconButton aria-label="Add to favorites">
-                            <FavoriteBorderIcon />
-                        </IconButton>
-                        <IconButton
-                            className={classnames(classes.expand, {
-                            [classes.expandOpen]: this.state.expanded,
-                            })}
-                            onClick={this.handleExpandClick}
-                            aria-expanded={this.state.expanded}
-                            aria-label="Show more"
-                            name={char._id}
-                        >
-                            <ExpandMoreIcon />
-                        </IconButton>
-                    </CardActions>
-                    <Collapse
-                        in={this.state.expanded}
-                        transitionDuration="auto"
-                        unmountOnExit
-                    >
-                        <CardContent>
-                            <Divider light />
-                            <Typography paragraph type="body2" className={classes.info}>
-                                {aceBonus} {char.ace}
-                            </Typography>
-                            <Divider light />
-                            <Typography paragraph type="body2" className={classes.info}>
-                                {mindCommand} {char.mind} <br />
-                                {twinCommand} {char.twin}
-                            </Typography>
-                            <Divider light />
-                        </CardContent>
-                    </Collapse>
-                </Card>
-            </div>      
-        );
-    }
+    const char = this.props.data;
+    char.mind = char.mind.map((mind, i) => {
+      return <span key={i}> {mind} </span>;
+    });
+
+    return (
+      // TODO: MAKE CHAR DETAIL COMPONENT AND TRANSFER KEY PROPS
+      <div>
+        <Card className={classes.card}>
+          <CardActions>
+            <Typography paragraph type="headline" className={classes.title}>
+              {char.name}
+            </Typography>
+            <div className={classes.flexGrow} />
+            <IconButton aria-label="Add to favorites">
+              <FavoriteBorderIcon />
+            </IconButton>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded,
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+              name={char._id}
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>
+            <CardContent>
+              <Divider light />
+              <Typography paragraph type="body2" className={classes.info}>
+                {aceBonus} {char.ace}
+              </Typography>
+              <Divider light />
+              <Typography paragraph type="body2" className={classes.info}>
+                {mindCommand} {char.mind} <br />
+                {twinCommand} {char.twin}
+              </Typography>
+              <Divider light />
+            </CardContent>
+          </Collapse>
+        </Card>
+      </div>
+    );
+  }
 }
 
 CharDetail.propTypes = propTypes;
--- client/components/Mechanic/MechDetail.js
+++ client/components/Mechanic/MechDetail.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { withStyles } from 'material-ui/styles';
-import Card, { CardHeader ,CardActions, CardContent } from 'material-ui/Card';
+import Card, { CardHeader, CardActions, CardContent } from 'material-ui/Card';
 import Collapse from 'material-ui/transitions/Collapse';
 import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton'
+import IconButton from 'material-ui/IconButton';
 import FavoriteBorderIcon from 'material-ui-icons/FavoriteBorder';
 import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 import Typography from 'material-ui/Typography';
@@ -15,107 +15,107 @@ import { red } from 'material-ui/colors';
 const accent = red['A400']; // #FF1744
 
 const styles = theme => ({
-    card: {
-        marginLeft: 4,
-        marginTop: 7,
-        minWidth: 275,
-    },
-    title: {
-        marginLeft: 20,
-        marginTop: 14,
-        fontSize: 17,
-    },
-    infoname: {
-        fontSize: 15,
-        color: accent,
-        marginTop: 10,
-    },
-    info: {
-        fontSize: 15,
-        marginTop: 10,
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        transition: theme.transitions.create('transform', {
-          duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    flexGrow: {
-        flex: '1 1 auto',
-    },
+  card: {
+    marginLeft: 4,
+    marginTop: 7,
+    minWidth: 275,
+  },
+  title: {
+    marginLeft: 20,
+    marginTop: 14,
+    fontSize: 17,
+  },
+  infoname: {
+    fontSize: 15,
+    color: accent,
+    marginTop: 10,
+  },
+  info: {
+    fontSize: 15,
+    marginTop: 10,
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  flexGrow: {
+    flex: '1 1 auto',
+  },
 });
 
 const propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 const defaultProps = {
-    classes: undefined,
+  classes: undefined,
 };
 
 class MechDetail extends Component {
-    state = { expanded: false };
-    
-    handleExpandClick = () => {
-        this.setState({ expanded: !this.state.expanded });
-    };
+  state = { expanded: false };
 
-    render() {
-        const classes = this.props.classes;
-        const fullBonus = <span className={classes.infoname}>FB: </span>
-        const movePoint = <span className={classes.infoname}>이동: </span>
-        const skill = <span className={classes.infoname}>특수: </span>
-        const geoState = <span className={classes.infoname}>지형: </span>
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
 
-        const mech = this.props.data;
-        mech.skill = mech.skill.map((skill, i) => {
-            return (<span key={i}> {skill} </span>);
-        })
+  render() {
+    const classes = this.props.classes;
+    const fullBonus = <span className={classes.infoname}>FB: </span>;
+    const movePoint = <span className={classes.infoname}>이동: </span>;
+    const skill = <span className={classes.infoname}>스킬: </span>;
+    const geoState = <span className={classes.infoname}>지형: </span>;
 
-        return (
-            <div className={this.props.classes.root}>
-                <Card className={classes.card}>
-                    <CardActions>
-                        <Typography paragraph type="headline" className={classes.title}>
-                            {mech.name}
-                        </Typography>
-                        <div className={classes.flexGrow} />
-                        <IconButton aria-label="Add to favorites">
-                            <FavoriteBorderIcon />
-                        </IconButton>
-                        <IconButton
-                            className={classnames(classes.expand, {
-                            [classes.expandOpen]: this.state.expanded,
-                            })}
-                            onClick={this.handleExpandClick}
-                            aria-expanded={this.state.expanded}
-                            aria-label="Show more"
-                        >
-                            <ExpandMoreIcon />
-                        </IconButton>
-                    </CardActions>
-                    <Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>
-                        <CardContent>
-                            <Divider light />
-                            <Typography paragraph type="body2" className={classes.info}>
-                                {fullBonus} {mech.full}
-                            </Typography>
-                            <Divider light />
-                            <Typography paragraph type="body2" className={classes.info}>
-                                {movePoint} {mech.move} <br />
-                                {skill} {mech.skill} <br />
-                                {geoState} {mech.geo}
-                            </Typography>
-                            <Divider light />
-                        </CardContent>
-                    </Collapse>
-                </Card>
-          </div>
-        );
-    }
+    const mech = this.props.data;
+    mech.skill = mech.skill.map((skill, i) => {
+      return <span key={i}> {skill} </span>;
+    });
+
+    return (
+      <div className={this.props.classes.root}>
+        <Card className={classes.card}>
+          <CardActions>
+            <Typography paragraph type="headline" className={classes.title}>
+              {mech.name}
+            </Typography>
+            <div className={classes.flexGrow} />
+            <IconButton aria-label="Add to favorites">
+              <FavoriteBorderIcon />
+            </IconButton>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded,
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={this.state.expanded} transitionDuration="auto" unmountOnExit>
+            <CardContent>
+              <Divider light />
+              <Typography paragraph type="body2" className={classes.info}>
+                {fullBonus} {mech.full}
+              </Typography>
+              <Divider light />
+              <Typography paragraph type="body2" className={classes.info}>
+                {movePoint} {mech.move} <br />
+                {skill} {mech.skill} <br />
+                {geoState} {mech.geo}
+              </Typography>
+              <Divider light />
+            </CardContent>
+          </Collapse>
+        </Card>
+      </div>
+    );
+  }
 }
 
 MechDetail.propTypes = propTypes;
